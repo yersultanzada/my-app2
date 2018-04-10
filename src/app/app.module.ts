b507ebdd842d8c29b8e243f693dfcdb99424d7d4
yersultanzada <yersultan.zada@gmail.com>
2018-04-10 17:42:58 +0600

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BackgroundDirective } from './directives/background.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundDirective
   ],
   imports: [
     BrowserModule
