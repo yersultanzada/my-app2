f924c606e589806d50d6e57b480796d5f32c8c7a
yersultanzada <yersultan.zada@gmail.com>
2018-04-14 21:58:04 +0600

    Конец блока 4. Директивы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   headerText = 'Создание своей директивы';
+
+  items = [1, 2, 3, 4, 5];
+  current = 1;
+
+  onClick(number: number) {
+    this.current = number;
+  }
 }
--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,29 +1,27 @@
-import {Directive, ElementRef, OnInit, Renderer2, HostListener} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 export class BackgroundDirective implements OnInit {
+  @Input('appBackground') hoverColor;
+  @Input() defaultColor;
+
+  @HostBinding('style.backgroundColor') background: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit() {
-
+    this.background = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseEnter() {
-    const {nativeElement} = this.element;
-
-    this.renderer.setStyle(nativeElement, 'background-color', 'blue');
-    this.renderer.addClass(nativeElement, 'white-text');
+    this.background = this.hoverColor;
   }
 
   @HostListener('mouseleave') mouseLeave() {
-    const {nativeElement} = this.element;
-
-    this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
-    this.renderer.removeClass(nativeElement, 'white-text');
+    this.background = this.defaultColor;
   }
 }
 
