a1cea7d0367e4726255c0ecd048950c9b29cf104
yersultanzada <yersultan.zada@gmail.com>
2018-04-16 14:20:34 +0600

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,42 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="col-md-12">
+      <h2>{{ name }}</h2>
+      <h2>{{ name | uppercase }}</h2>
+      <h2>{{ name | slice:0:3 }}</h2>
+      <hr>
+      <h2>{{ pi }}</h2>
+      <h2>{{ pi | number }}</h2>
+      <h2>{{ pi | number:'2.2-2' }}</h2>
+      <hr>
+      <h2>{{ money | currency:'USD' }}</h2>
+      <hr>
+      <h2>{{ date }}</h2>
+      <h2>{{ date | date }}</h2>
+      <h2>{{ date | date: 'fullDate' }}</h2>
+      <hr>
+      <h2>{{ amount | percent }}</h2>
+      <h2>{{ object }}</h2>
+      <h2><pre>{{ object | json }}</pre></h2>
+    </div>
+  `
 })
 export class AppComponent {
-  headerText = 'Создание своей директивы';
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested:
+      {
+        xyz: 3,
+        numbers: [1, 2, 3]
+      }
+  };
 
-  items = [1, 2, 3, 4, 5];
-  current = 1;
-
-  onClick(number: number) {
-    this.current = number;
-  }
 }
