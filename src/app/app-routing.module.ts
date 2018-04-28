237e1257b6f1321f91d3a760b35fd9645c5dcec7
yersultanzada <yersultan.zada@gmail.com>
2018-04-28 08:16:19 +0600

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
+import { CarPageComponent } from './car-page/car-page.component';
 
 const appRoutes: Routes = [
   // localhost:4200/cars
   { path: '', component: HomePageComponent},
-  { path: 'cars', component: CarsPageComponent}
+  { path: 'cars', component: CarsPageComponent},
+  { path: 'cars/:id/:name', component: CarPageComponent}
 ];
 
 @NgModule({
