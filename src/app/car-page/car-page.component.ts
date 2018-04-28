60f83f7dc3829626e1d3352656a17cd1adcdc71c
yersultanzada <yersultan.zada@gmail.com>
2018-04-28 12:55:24 +0600

    .

--- src/app/car-page/car-page.component.ts
+++ src/app/car-page/car-page.component.ts
@@ -10,12 +10,19 @@ export class CarPageComponent implements OnInit {
 
   id: number;
   name: string;
+  year: string;
+  color: string;
+  hash: string;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // когда обращаемся в снепшот мы забираем данные только 1 раз
     this.id = +this.route.snapshot.params['id'];
     this.name = this.route.snapshot.params['name'];
+    this.color = this.route.snapshot.queryParams['color'];
+    this.year = this.route.snapshot.queryParams['year'];
+    this.hash = this.route.snapshot.fragment;
 
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -5,19 +5,27 @@ export class CarsService {
   cars = [
     {
       name: 'Ford',
-      id: 1
+      id: 1,
+      year: 2004,
+      color: 'blue'
     },
     {
       name: 'Audi',
-      id: 2
+      id: 2,
+      year: 2014,
+      color: 'black'
     },
     {
       name: 'BMW',
-      id: 3
+      id: 3,
+      year: 2007,
+      color: 'yellow'
     },
     {
       name: 'Mercedes',
-      id: 4
+      id: 4,
+      year: 2010,
+      color: 'red'
     }
   ];
 }
