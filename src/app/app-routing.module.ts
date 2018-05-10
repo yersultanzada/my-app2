b315fcee9b311c638678f548ffe13a69bd2a6449
yersultanzada <yersultan.zada@gmail.com>
2018-05-10 14:23:46 +0600

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CarsPageComponent } from './cars-page/cars-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { CarPageComponent } from './cars-page/car-page/car-page.component';
 
 const appRoutes: Routes = [
-  { path: 'cars', component: CarsPageComponent, children: [
-      { path: ':id/:name', component: CarPageComponent }
-    ] },
   { path: '', component: HomePageComponent }
 ];
 
--- src/app/cars-page/cars.module.ts
+++ src/app/cars-page/cars.module.ts
@@ -4,6 +4,7 @@ import {CommonModule} from '@angular/common';
 import {CarsPageComponent} from './cars-page.component';
 import {CarPageComponent} from './car-page/car-page.component';
 import {CarsService} from '../cars.service';
+import {CarsRoutingModule} from './cars-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {CarsService} from '../cars.service';
     CarPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CarsRoutingModule
   ],
   providers: [CarsService]
 })
