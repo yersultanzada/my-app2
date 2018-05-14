b16eb16b2d4739fee60a625cf0440293f269e0a8
yersultanzada <yersultan.zada@gmail.com>
2018-05-14 14:09:56 +0600

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,61 +11,9 @@ import {
   selector: 'app-root',
   templateUrl: './app.component.html',
   animations: [
-    trigger('clickedDiv', [
-      state('start', style({
-        backgroundColor: 'blue',
-        width: '150px',
-        height: '150px'
-      })),
-      state('end', style({
-        backgroundColor: 'red',
-        width: '300px',
-        height: '300px'
-      })),
-      state('active', style({
-        backgroundColor: 'orange',
-        width: '170px',
-        height: '170px'
-      })),
-      transition('start <=> end', animate('800ms ease-in')),
-      transition('start => active', animate(400)),
-      transition('active => end', animate(400))
-    ]),
-    trigger('multi', [
-      state('start', style({
-        width: '150px',
-        height: '150px',
-        border: '5px solid black'
-      })),
-      state('end', style({
-        width: '170px',
-        height: '170px',
-        background: 'blue'
-      })),
-      transition('start <=> end', [
-        style({
-          background: 'red'
-        }),
-        animate(1500, style({
-          background: 'yellow'
-        })),
-        animate(1000, style({
-          width: '200px',
-          height: '200px'
-        })),
-        animate(1000)
-      ])
-    ])
+
   ]
 })
 export class AppComponent {
-  clickedDivState = 'start';
-
-  multiState = 'start';
-  changeDivState() {
-    this.clickedDivState = 'end';
-    setTimeout(() => {
-      this.clickedDivState = 'start';
-    }, 3000);
-  }
+  isVisible = false;
 }
