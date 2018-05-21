f911d603193eb1a0da22193a17c79519af351717
yersultanzada <yersultan.zada@gmail.com>
2018-05-21 14:53:04 +0600

    Unit test beginning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
-import {changeWidthTrigger, divTrigger} from './app.animations';
-import { AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [divTrigger, changeWidthTrigger]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isVisible = false;
-
-  onAnimationStart(event: AnimationEvent) {
-    console.log('Start: ', event);
-  }
-
-  onAnimationDone(event: AnimationEvent) {
-    console.log('Done: ', event);
-  }
+  title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,8 +12,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    BrowserAnimationsModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
